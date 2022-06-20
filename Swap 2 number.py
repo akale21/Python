9ef34c2786686e20295ec89ba58bb6315a50fd38
akale21 <78691845+akale21@users.noreply.github.com>
2022-06-20 12:06:12 +0530

    Add files via upload

---
+++ Swap 2 number.py
@@ -0,0 +1,10 @@
+x = 12
+y = 34
+
+print("before swapping: ")
+print("first number is: ", x, "Second number is: ", y)
+
+x, y = y, x
+
+print("after swapping: ")
+print("first number is: ", x, "Second number is: ", y)
